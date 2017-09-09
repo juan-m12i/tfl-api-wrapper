e6dae27cf1eb735369d0cdf144539112d332166d
Juan Montalvo Bressi <juanmontalvobressi@gmail.com>
2017-09-09 13:33:49 +0100

    change config

--- cfg/config.py
+++ cfg/config.py
@@ -1,5 +1,7 @@
-credentials = {
-    "app_id": app_id,
-    "app_key": app_key,
- }
- 
+import json
+
+def get_credentials():
+    with open('config.cfg') as data_file:    
+        data = json.load(data_file)
+    return data["credentials"]
+
